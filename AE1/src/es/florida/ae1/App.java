9406ddbc6dfc3fc0f42d4ae56166357a1cfa67b0
Alex Alba <cem20601@gmail.com>
2021-09-20 07:21:15 +0200

    AE1 terminada

--- AE1/src/es/florida/ae1/App.java
+++ AE1/src/es/florida/ae1/App.java
@@ -6,24 +6,32 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class App {
-	public static void getInformacion(File f) {
-		if(f.isFile()){
-			System.out.println("El parámetro pasado es un archivo llamado "+f.getName());
-			System.out.println("El tamaño del archivo es: "+f.length()+" bytes");
+	public static void getInformacion() {
+		Scanner entrada=new Scanner(System.in);	
+		System.out.print("Introduce el nombre del fichero/carpeta que quieres consultar ");
+		File f=new File(entrada.next());
+		if(f.exists() || f.canRead())
+		{
+			if(f.isFile()){
+				System.out.println("- El parámetro pasado es un archivo llamado "+f.getName());
+				System.out.println("- El tamaño del archivo es: "+f.length()+" bytes");
+				}
+			else {
+				System.out.println("- El parámetro pasado es un directorio llamado "+f.getName());
+				String[] elementos=f.list();
+				System.out.println("- La carpeta contiene "+elementos.length+" elementos");
+				System.out.println("- Espacio libre: "+f.getFreeSpace()+" Espacio disponible: "+f.getUsableSpace()+" Espacio total: "+f.getTotalSpace());
 			}
-		else {
-			System.out.println("El parámetro pasado es un directorio llamado "+f.getName());
-			String[] elementos=f.list();
-			System.out.println("La carpeta contiene "+elementos.length+" elementos");
-			System.out.println("Espacio libre: "+f.getFreeSpace()+" Espacio disponible: "+f.getUsableSpace()+" Espacio total: "+f.getTotalSpace());
+			
+			Date fecha= new Date(f.lastModified());
+			System.out.println("- La ruta absoluta es: "+f.getAbsolutePath());
+			System.out.println("- Fecha de la última modificación: "+fecha);
+			
+			if (f.isHidden())System.out.println("- El archivo está oculto.");
+			else System.out.println("- El archivo NO está oculto.");
 		}
+		else System.out.println("El fichero/carpeta no existe o no es accesible, volviendo al menú principal...");
 		
-		Date fecha= new Date(f.lastModified());
-		System.out.println("La ruta absoluta es: "+f.getAbsolutePath());
-		System.out.println("Fecha de la última modificación: "+fecha);
-		
-		if (f.isHidden())System.out.println("El archivo está oculto.");
-		else System.out.println("El archivo NO está oculto.");
 	}
 	
 	public static void creaCarpeta() {
@@ -31,32 +39,35 @@ public class App {
 		System.out.print("Introduce el nombre de la carpeta que quieres crear ");
 		//creamos variable tipo file con el nombre que ha introducido el usuario
 		File nuevaCarpeta=new File(entrada.next());
-		try{nuevaCarpeta.mkdir();}
-		catch (Exception e){e.getMessage();}
 		
-		System.out.println("Carpeta creada correctamente");
-		entrada.close();
+		if(nuevaCarpeta.exists() || !nuevaCarpeta.canWrite()) System.out.println("La carpeta ya existe o no es accesible, volviendo al menú principal...");
+		else {
+			try{nuevaCarpeta.mkdir();}
+			catch (Exception e){e.getMessage();}
+			System.out.println("Carpeta creada correctamente");
+		}
 	}
 	
 	public static void creaFichero() {
 		Scanner entrada=new Scanner(System.in);	
 		System.out.print("Introduce el nombre del fichero que quieres crear ");
 		File nuevoFichero=new File(entrada.next());
+		
+		if(nuevoFichero.exists()) System.out.println("El archivo ya existe, volviendo al menú principal...");
+		else {
 		try {nuevoFichero.createNewFile();} 
 		catch (IOException e) {System.out.println("No se ha podido crear el fichero");}
-		
 		System.out.println("Fichero creado correctamente");
-		entrada.close();
+		}
 	}
 	
 	public static void elimina() {
 		Scanner entrada=new Scanner(System.in);	
 		System.out.print("Introduce el nombre del fichero/carpeta que quieres eliminar ");
 		File nombre=new File(entrada.next());
-		if(nombre.delete())System.out.println("Elemento eliminado correctamente");
 		
+		if(nombre.delete())System.out.println("Elemento eliminado correctamente");
 		else System.out.println("El elemento no se ha podido eliminar");
-		entrada.close();
 	}
 	
 	public static void renombra() {
@@ -66,23 +77,20 @@ public class App {
 		System.out.print("Introduce el nuevo nombre ");
 		File newFile=new File(entrada_resp.next());
 		oldFile.renameTo(newFile);
-		
-		entrada_resp.close();
 	}
 
 	public static void main(String[] args) {		
-		//inicialmente usaremos variables definidas, después las capturaremos de los argumentos
 		Scanner entrada=new Scanner(System.in);	
-		File carpeta = new File("carpeta");
-		File archivo=new File("carpeta/test.txt");
 		boolean menu=true;
 		int resp;
+		
 		while(menu==true) {
-			System.out.println("\n Introduce el número de comando que deseas ejecutar: \n 1: Mostrar información \n 2: Crear una carpeta \n 3: Crear un fichero \n 4: Eliminar un fichero/carpeta \n 5: Renombrar un archivo/carpeta \n 6: Salir" );
+			System.out.println("\n******Gestión de archivos******");
+			System.out.println("Introduce el número de comando que deseas ejecutar: \n 1: Mostrar información \n 2: Crear una carpeta \n 3: Crear un fichero \n 4: Eliminar un fichero/carpeta \n 5: Renombrar un archivo/carpeta \n 6: Salir" );
 			resp=entrada.nextInt();
 			switch(resp) {
 			case 1: 
-				getInformacion(carpeta);
+				getInformacion();
 				break;
 			case 2: 
 				creaCarpeta();
@@ -98,10 +106,13 @@ public class App {
 				break;
 			case 6:
 				menu=false;
+				System.out.println("Saliendo...");
+				entrada.close();
 				break;
 			default:
 				System.out.println("Introduce una opción válida \n 1: Mostrar información \n 2: Crear una carpeta \n 3: Crear un fichero \n 4: Eliminar un fichero/carpeta \n 5: Renombrar un archivo/carpeta \n 6: Salir");
 				resp=entrada.nextInt();
+				entrada.close();
 				break;
 			}
 		}
